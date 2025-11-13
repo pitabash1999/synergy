e49c534332f2f3f4ee894d7b888792f38f7b18ae
Pitabash <pitabashk196@gmail.com>
2025-11-13 16:41:18 +0530

    corrected the not found page

--- user-management/src/components/NotFound.jsx
+++ user-management/src/components/NotFound.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>not found</div>;
+  return (
+    <div className="flex flex-col items-center justify-center text-2xl font-bold min-h-screen fixed inset-0">
+      404 <span>NOT FOUND</span>
+      But don't worry, you can always find your way back{" "}
+      <a href="/" className="text-blue-600 underline">
+        home
+      </a>
+      !
+    </div>
+  );
 };
 
 export default NotFound;
